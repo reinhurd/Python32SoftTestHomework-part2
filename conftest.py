41d03c2317d2f83deb8629b65fcf5bad25c719a4
Reinhurd <roarus@yandex.ru>
2018-12-24 01:46:04 +0400

    Изменение проекта по состоянию на видео 9-2

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from fixture.application import Application
 import json
 import os.path
+import ftputil
 
 fixture = None
 target = None
@@ -16,19 +17,48 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    webconfig = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, baseurl=webconfig['baseurl'])
+        fixture = Application(browser=browser, baseurl=config['web']['baseurl'])
     return fixture
 
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.back"):
+            remote.remove("config_inc.php.back")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.back")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resourses/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.back"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.back", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
